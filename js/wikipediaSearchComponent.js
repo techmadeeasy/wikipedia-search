47a7c65789dea62f0b8981beb765b608830597d7
jerryboy <djerryboy2@gmail.com>
2021-08-17 21:47:37 +0200

    wikipedia search

--- js/wikipediaSearchComponent.js
+++ js/wikipediaSearchComponent.js
@@ -8,7 +8,7 @@ export function WikipediaSearch() {
        let listElement =  document.querySelector('#wikipedia-search-result');
         for(const [index, result] of results.pages.entries()){
             if (index === results.pages.length - 1){
-                listElement.innerHTML += `<li><h3> ${ result.title } </h3><input type='text' id='' placeholder='Search for pages containing [x]...'></li>`
+                listElement.innerHTML += `<li><h3> ${ result.title } </h3><input type='text' placeholder='Search for pages containing [x]...'></li>`
                 return;
             }
             listElement.innerHTML += `<li> <img src='${ result.thumbnail ? result.thumbnail.url : 'img/placeholder.png'}' alt='image' width='100'> <div class='block-content'><h3>${result.title}</h3> <div>${result.description ?? '...'}</div> </div></li>`
